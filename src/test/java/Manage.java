94917fb0d21fafcaa89dd9a128ed55aad4f00c90
jynxxy <slawekpiesko@gmail.com>
2022-10-17 23:26:09 +0200

    added gettingAndDeleteCookies

--- src/test/java/Manage.java
+++ src/test/java/Manage.java
@@ -1,7 +1,9 @@
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Cookie;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,10 +18,15 @@ public class Manage {
     public void driverSetup() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         driver = new ChromeDriver();
-        //  driver.manage().window().setSize(new Dimension(1280, 720));
-        driver.manage().window().setPosition(new Point(-1000, 0));
-        driver.manage().window().maximize();
+        //driver.manage().window().setSize(new Dimension(1280, 720));
+        driver.manage().window().setPosition(new Point(1000, 0));
+        // driver.manage().window().maximize();
         driver.navigate().to("https://www.amazon.com");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterEach
@@ -29,8 +36,17 @@ public class Manage {
     }
 
     @Test
-    public void gettingCookies() {
-        Set<Cookie> cookies = driver.manage().getCookies();
+    public void gettingAndDeletingCookies() {
+        Assertions.assertEquals(7, driver.manage().getCookies().size(), "Number of cookies is not what expected");
         Cookie cookieSessionId = driver.manage().getCookieNamed("session-id");
+        driver.manage().deleteCookieNamed("seassion-id");
+        Assertions.assertEquals(6, driver.manage().getCookies().size(), "Number of cookies is not what expected");
+        driver.manage().deleteAllCookies();
+        Assertions.assertEquals(0, driver.manage().getCookies().size(), "Number of cookies is not what expected");
+    }
+
+    @Test
+    public void addingCookies() {
+
     }
 }
