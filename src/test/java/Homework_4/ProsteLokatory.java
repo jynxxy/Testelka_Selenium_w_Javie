6586b4b0d6398bc4425159d52dc895ea76f35f38
spiesko <spiesko@sii.pl>
2022-10-21 16:06:44 +0200

    added ProsteLokatory.java

---
+++ src/test/java/Homework_4/ProsteLokatory.java
@@ -0,0 +1,31 @@
+package Homework_4;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ProsteLokatory {
+
+    WebDriver driver;
+
+    @BeforeEach
+    public void driverSetup() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1290, 730));
+        driver.manage().window().setPosition(new Point(8, 30));
+        driver.navigate().to("http://fakestore.testelka.pl/moje-konto");
+    }
+
+    @AfterEach
+    public void closeAndQuit() {
+        //    driver.close();
+        driver.quit();
+    }
+
+
+    
+}
